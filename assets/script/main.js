73dba9990e2d3a8ccedb274e07832147e7c065b7
ina <r.ina.kimiduka@gmail.com>
2025-08-08 22:12:14 +0900

    初稿提出

--- assets/script/main.js
+++ assets/script/main.js
@@ -7,37 +7,20 @@ $(document).ready(function () {
 		slidesToShow: 3,
 		slidesToScroll: 3,
 		responsive: [{
-			breakpoint: 767,
+			breakpoint: 767.96,
 			settings: {
 				slidesToShow: 1,
 				slidesToScroll: 1,
 			}
 		}]
 	});
-	// if (window.matchMedia('(min-width: 768px)').matches) {
-	// 	$('#js-voices-carousel').slick({
-	// 		dots: true,
-	// 		slidesToShow: 3,
-	// 		slidesToScroll: 3,
-	// 		responsive: [{
-	// 			breakpoint: 767,
-	// 			settings: {
-	// 				slidesToShow: 1
-	// 			}
-	// 		}]
-	// 	});
-	// } else {
-	// 	$('#js-voices-carousel').slick({
-	// 		dots: true,
-	// 	});
-	// }
 
 	// スクロールボタン表示制御
 	$(window).scroll(function() {
-		if($(this).scrollTop()) {
-			$('#js-scroll-to-top-btn').show();
+		if($(this).scrollTop() > 200) {
+			$('#js-scroll-to-top-btn').fadeIn();
 		}else{
-			$('#js-scroll-to-top-btn').hide();
+			$('#js-scroll-to-top-btn').fadeOut();
 		}
 	});
 });
