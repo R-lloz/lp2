1cbf533eb0d0b258be8b89229dbdf1b3c9a64f42
ina <r.ina.kimiduka@gmail.com>
2025-10-02 10:30:01 +0900

    修正稿提出

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,26 +1,26 @@
 'use strict';
 
 $(document).ready(function () {
-	// カルーセル
-	$('#js-voices-carousel').slick({
-		dots: true,
-		slidesToShow: 3,
-		slidesToScroll: 3,
-		responsive: [{
-			breakpoint: 767.96,
-			settings: {
-				slidesToShow: 1,
-				slidesToScroll: 1,
-			}
-		}]
-	});
-
-	// スクロールボタン表示制御
-	$(window).scroll(function() {
-		if($(this).scrollTop() > 200) {
-			$('#js-scroll-to-top-btn').fadeIn();
-		}else{
-			$('#js-scroll-to-top-btn').fadeOut();
-		}
-	});
+    // カルーセル
+    $('#js-voices-carousel').slick({
+        dots: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [{
+            breakpoint: 767.96,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }]
+    });
+    
+    // スクロールボタン表示制御
+    $(window).scroll(function() {
+        if($(this).scrollTop() > 200) {
+            $('#js-scroll-to-top-btn').fadeIn();
+        }else{
+            $('#js-scroll-to-top-btn').fadeOut();
+        }
+    });
 });
